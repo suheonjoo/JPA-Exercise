e2d9fd5822bdb9fd093e9a3905cb9233006856bb
suheonjoo <suheonj95@naver.com>
2022-02-24 13:21:47 +0900

    220224

---
+++ JPA 기본/jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,30 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        //jpa의 모등 데이터 변경은 무조건 transaction안에서 일어나야 한다
+        tx.begin();
+
+        try {
+
+
+            tx.commit();
+       }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+        //was가 내려갈때 팩토리를 닫아줘야한, 그래야 리소스들이 릴리즈 된다
+    }
+}
