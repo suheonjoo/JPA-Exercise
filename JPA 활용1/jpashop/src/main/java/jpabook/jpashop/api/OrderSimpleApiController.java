258933db39e2b5223735d957a6e5de56bf902cd1
suheonjoo <suheonj95@naver.com>
2022-03-17 02:26:31 +0900

    220317

---
+++ JPA 활용1/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne
+ * Order
+ * Order -> Member
+ * Order -> Deliver
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findALlByString(new OrderSearch());
+        return all;
+    }
+
+}
