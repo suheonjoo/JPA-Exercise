6acb57fdb6557a444a4a02373585df468e425b2c
suheonjoo <suheonj95@naver.com>
2022-03-21 13:17:33 +0900

    220321

--- JPA 실전/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ JPA 실전/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,7 +1,21 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username,int age);
+
+    List<Member> findTop3HelloBy();
+
+    //@Query(name = "Member.findByUsername") //이거 없어도 알아서 해결해줌
+    List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
--- JPA 실전/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ JPA 실전/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
@@ -14,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+//@Rollback(value = false)
 class MemberJpaRepositoryTest {
 
     @Autowired MemberJpaRepository memberJpaRepository;
@@ -45,6 +47,9 @@ class MemberJpaRepositoryTest {
         assertThat(findMember1).isEqualTo(member1);
         assertThat(findMember2).isEqualTo(member2);
 
+        //update체크용
+        //findMember1.setUsername("member!!!!!");
+
         //리스트 조회검증
         List<Member> all = memberJpaRepository.findAll();
         assertThat(all.size()).isEqualTo(2);
@@ -60,6 +65,34 @@ class MemberJpaRepositoryTest {
         assertThat(deletedCount).isEqualTo(0);
 
     }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberJpaRepository.save(m1);
+        memberJpaRepository.save(m2);
+
+        List<Member> result = memberJpaRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void testNamedQuery(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberJpaRepository.save(m1);
+        memberJpaRepository.save(m2);
+
+        List<Member> result = memberJpaRepository.findByUsername("AAA");
+        Member findMember = result.get(0);
+        assertThat(findMember).isEqualTo(m1);
+    }
+
+
 }
 
 
